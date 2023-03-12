5e1f91f9be370aa7952d080132118b1c91976387
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-12 10:44:18 +0000

    ddd

--- app.py
+++ app.py
@@ -60,6 +60,7 @@ def main():
         for i, layout in enumerate(layouts):
             st.write(f"Page {i+1}")
             st.write(layout)
+            layoutparser.visualization.draw_text(canvas, layout, arrangement: str = 'lr')
             # st.image(layout.to_image(), caption=f"Page {i+1} layout", use_column_width=True)
 
 if __name__ == "__main__":
