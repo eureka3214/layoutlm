a092c18fe95a95f4e8bd636d83e1ef88ca0ee84d
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-12 11:57:11 +0000

    ccc

--- pages/layoutlm.py
+++ pages/layoutlm.py
@@ -10,8 +10,11 @@ pdf_file = st.file_uploader("Upload a PDF file", type="pdf")
 if pdf_file is not None:
     pdf_doc = fitz.open(stream=pdf_file.read(), filetype="pdf")
     pdf_page = pdf_doc[0]
+    png_bytes = pdf_page.get_pixmap().png
+    with open("output.png", "wb") as f:
+        f.write(png_bytes)
     # Read the contents of the file
     # pdf_contents = pdf_file.read()
     layouts = lp.io.load_pdf(pdf_file)
     # Do something with the PDF contents
-    st.write(layouts)
+    # st.write(layouts)
