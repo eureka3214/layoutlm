1048bc0ef744819ea110cbe64ec6915d6effa54f
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-12 12:02:07 +0000

    mmm

--- pages/layoutlm.py
+++ pages/layoutlm.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import layoutparser as lp
 import fitz
+from io import BytesIO
+from PIL import Image
+
 
 
 # Upload PDF file
@@ -11,8 +14,22 @@ if pdf_file is not None:
     pdf_doc = fitz.open(stream=pdf_file.read(), filetype="pdf")
     pdf_page = pdf_doc[0]
     png_bytes = pdf_page.get_pixmap()
-    with open("output.png", "wb") as f:
-        f.write(png_bytes)
+
+
+
+    pil_image = Image.frombytes(
+    mode="RGB",
+    size=pdf_page.pixmap.width_height,
+    data=pdf_page.pixmap.samples)
+
+    # Convert the PIL Image object to bytes
+    bytes_io = BytesIO()
+    pil_image.save(bytes_io, format="PNG")
+    png_bytes = bytes_io.getvalue()
+    st.image(png_bytes, caption="PDF Page 1 as PNG")
+
+    # with open("output.png", "wb") as f:
+    #     f.write(png_bytes)
     # Read the contents of the file
     # pdf_contents = pdf_file.read()
     layouts = lp.io.load_pdf(pdf_file)
