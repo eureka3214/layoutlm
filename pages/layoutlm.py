8d5260b3516fae465f4aafed4d98bbcca6b8df13
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-12 11:50:05 +0000

    mm

--- pages/layoutlm.py
+++ pages/layoutlm.py
@@ -3,19 +3,6 @@ import layoutparser as lp
 import fitz
 
 
-
-
-# Select the first page of the PDF
-# pdf_page = pdf_doc[0]
-
-# # Convert the PDF page to a PNG image
-# png_bytes = pdf_page.getPixmap().png
-
-# # Write the PNG image to a file
-# with open("output.png", "wb") as f:
-#     f.write(png_bytes)
-
-
 # Upload PDF file
 pdf_file = st.file_uploader("Upload a PDF file", type="pdf")
 
