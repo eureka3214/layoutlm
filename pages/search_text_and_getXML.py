9e0ca40741f09cb135c3abe8dea9b1b6cff11f80
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-15 06:22:12 +0000

    ddd

--- pages/search_text_and_getXML.py
+++ pages/search_text_and_getXML.py
@@ -10,7 +10,7 @@ def display_blocks(pdf_path):
     blocks = page.get_text("dict")["blocks"]
     for b in blocks:  # iterate through the text blocks
         for l in b["lines"]:  # iterate through the text lines
-            st.write(l)
+            st.write(l["text"])
     
     # create radio buttons for each block
     block_index = st.radio("Select a block:", [i for i in range(len(blocks))])
