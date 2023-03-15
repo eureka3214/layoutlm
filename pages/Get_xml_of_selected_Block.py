dd37bd634778cca96ef60462b3e7bd385c39f451
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-15 07:21:59 +0000

    sss

--- pages/Get_xml_of_selected_Block.py
+++ pages/Get_xml_of_selected_Block.py
@@ -11,20 +11,20 @@ def display_blocks(pdf_path):
         # read page text as a dictionary, suppressing extra spaces in CJK fonts
         blocks = page.get_text("dict")["blocks"]
         for i, b in enumerate(blocks):  # iterate through the text blocks
-            with st.container(f"Block {i}"):
+            # with st.container(f"Block {i}"):
                 # iterate through the text lines in the block
-                for l in b["lines"]:
-                    # iterate through the text spans in the line
-                    for s in l["spans"]:
-                        text = s["text"]
-                        # create a button for the line
-                        # with st.expander(text):
-                        block_xml = page.get_text("xml", text)
-                            # with st.expander("Block XML"):
-                        st.write(text)
-                        # with st.container():
-                        with st.expander("XML"):
-                            st.code(block_xml)
+            for l in b["lines"]:
+                # iterate through the text spans in the line
+                for s in l["spans"]:
+                    text = s["text"]
+                    # create a button for the line
+                    # with st.expander(text):
+                    block_xml = page.get_text("xml", text)
+                        # with st.expander("Block XML"):
+                    st.write(text)
+                    # with st.container():
+                    with st.expander("XML"):
+                        st.code(block_xml)
 
 
 st.title("PDF get xml of selected block")
