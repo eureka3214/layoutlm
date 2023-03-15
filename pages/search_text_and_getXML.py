af1224638223ce209db1af64a18841df840d46b3
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-15 05:55:02 +0000

    sss

--- pages/search_text_and_getXML.py
+++ pages/search_text_and_getXML.py
@@ -2,22 +2,19 @@ import streamlit as st
 import fitz
 
 
-def display_block_with_text(pdf_path, search_text):
+def display_blocks(pdf_path):
     doc = fitz.open(stream=pdf_path.read(), filetype="pdf")
     page = doc[0]
 
     # read page text as a dictionary, suppressing extra spaces in CJK fonts
-    blocks = page.get_text("dict")
-
-    # search for the block containing the search text
-    for block in blocks["blocks"]:
-        if search_text in block["text"]:
-            # get the XML content of the block containing the search text
-            block_xml = page.get_text("xml", block=block)
-            st.write(block_xml)
-            break
-    else:
-        st.write(f"Paragraph with text '{search_text}' not found.")
+    blocks = page.get_text("dict")["blocks"]
+    
+    # create radio buttons for each block
+    block_index = st.radio("Select a block:", [i for i in range(len(blocks))])
+    
+    # get the XML content of the selected block
+    block_xml = page.get_text("xml", block=blocks[block_index])
+    st.write(block_xml)
 
 
 st.title("PDF get xml")
@@ -25,6 +22,4 @@ st.title("PDF get xml")
 uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
 
 if uploaded_file is not None:
-    search_text = st.text_input("Enter the search text for the paragraph:")
-    if search_text:
-        display_block_with_text(uploaded_file, search_text)
+    display_blocks(uploaded_file)
