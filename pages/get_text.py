dd22fcb5953d5bf1746f2ca8c1f577a2051b84d0
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-15 11:03:18 +0000

    mmm

--- pages/get_text.py
+++ pages/get_text.py
@@ -6,7 +6,7 @@ def display_blocks(pdf_path):
     doc = fitz.open(stream=pdf_path.read(), filetype="pdf")
     pgno = st.number_input("Input page number", min_value=0)
     if pgno:
-        page = doc[pgno]
+        page = doc[pgno].get_textpage()
 
     # read page text as a dictionary, suppressing extra spaces in CJK fonts
         blocks = page.extractTEXT()
