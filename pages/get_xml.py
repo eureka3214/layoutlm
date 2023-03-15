cd4ac139d7288cdfaa5c6ae8c754d39f15111aea
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-15 05:47:39 +0000

    sss

--- pages/get_xml.py
+++ pages/get_xml.py
@@ -1,24 +1,6 @@
 import streamlit as st
 import fitz
 
-def flags_decomposer(flags):
-    """Make font flags human readable."""
-    l = []
-    if flags & 2 ** 0:
-        l.append("superscript")
-    if flags & 2 ** 1:
-        l.append("italic")
-    if flags & 2 ** 2:
-        l.append("serifed")
-    else:
-        l.append("sans")
-    if flags & 2 ** 3:
-        l.append("monospaced")
-    else:
-        l.append("proportional")
-    if flags & 2 ** 4:
-        l.append("bold")
-    return ", ".join(l)
 
 def display_fonts(pdf_path):
     doc = fitz.open(stream=pdf_path.read(), filetype="pdf")
@@ -27,20 +9,7 @@ def display_fonts(pdf_path):
     # read page text as a dictionary, suppressing extra spaces in CJK fonts
     blocks = page.get_text("xml")
     st.write(blocks)
-    # for b in blocks:  # iterate through the text blocks
-    #     # lines = b["lines"]
-    #     st.write(b)
-    #     # for l in b["lines"]:  # iterate through the text lines
-        #     for s in l["spans"]:  # iterate through the text spans
-        #         st.write("")
-        #         font_properties = "Font: '%s' (%s), size %g, color #%06x" % (
-        #             s["font"],  # font name
-        #             flags_decomposer(s["flags"]),  # readable font flags
-        #             s["size"],  # font size
-        #             s["color"],  # font color
-        #         )
-        #         st.write("Text: '%s'" % s["text"])  # simple print of text
-        #         st.write(font_properties)
+  
 
 st.title("PDF get xml ")
 
