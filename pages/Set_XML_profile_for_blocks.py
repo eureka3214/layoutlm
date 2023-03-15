f9c3b5e58272f73c1a6a7f633c8a5a835347d20b
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-15 08:00:38 +0000

    ssss

---
+++ pages/Set_XML_profile_for_blocks.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import fitz
+
+def display_blocks(pdf_path):
+    doc = fitz.open(stream=pdf_path.read(), filetype="pdf")
+    pgno = st.number_input("Input page number", min_value=0)
+    if pgno:
+        page = doc[pgno]
+
+        # read page text as a dictionary, suppressing extra spaces in CJK fonts
+        blocks = page.get_text("dict")["blocks"]
+        for i, b in enumerate(blocks):  # iterate through the text blocks
+            selected_value = st.selectbox(f"Select a category for Text Block {i}", ['Category 1', 'Category 2', 'Category 3', 'Category 4'])
+            if st.checkbox("Expand Text Block"):
+                with st.beta_expander(f"Text Block {i} - {selected_value}"):
+                    # iterate through the text lines in the block
+                    for l in b["lines"]:
+                        # iterate through the text spans in the line
+                        for s in l["spans"]:
+                            text = s["text"]
+                            st.write(text)
+                st.write("Appending content to", selected_value)
+                # Append content to a list with the selected category name
+                st.session_state.setdefault(selected_value, []).append(b["text"])
+
+st.title("Set XML profile for selected blocks")
+
+uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
+
+if uploaded_file is not None:
+    display_blocks(uploaded_file)
+
+# Display the content for each category
+st.write("Content for each category:")
+for category, content_list in st.session_state.items():
+    if category not in ['_legacy_config', '_is_running']:
+        st.write(f"Category {category}:")
+        for content in content_list:
+            st.write(content)
